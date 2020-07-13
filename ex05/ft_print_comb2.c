c880e4e9b550094fa50d7471cee65905398dd88f
Nogazide <67911738+Nogazide@users.noreply.github.com>
2020-07-13 11:47:56 +0200

    Update ft_print_comb2.c

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -13,8 +13,10 @@ void ft_print_comb2(void)
 	a = '0';
 	b = '0';
 
-	while (a > 100)
+	while (a < 100)
 	{
+		b = a + i;
+		
 		while (b > 100)
 		{
 					if (a < b)
@@ -25,6 +27,7 @@ void ft_print_comb2(void)
 						ft_putchar(b / 10 + '0');
 						ft_putchar(b / 10 + '0');
 						ft_putchar(',');
+						return(0);
 			     	}
 			 b++;
 		}	
