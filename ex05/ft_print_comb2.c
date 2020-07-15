f05e275f814f0c567453f166d29019c0676ba616
Nogazide <67911738+Nogazide@users.noreply.github.com>
2020-07-15 14:20:50 +0200

    Update ft_print_comb2.c

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -1,9 +1,4 @@
-#include <unistd.h>
 
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void ft_print_comb2(void)
 {
@@ -36,8 +31,4 @@ void ft_print_comb2(void)
    
 }
 
-int main(void)
-{
-	ft_print_comb2();
-	return(0);
-}
+
